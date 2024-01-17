54f06792585ab649ecda508819ed12799a0e26b3
Nanlung <longnanlung@gmail.com>
2024-01-17 15:08:34 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import React, {useState} from 'react';
+import './App.css';
+
+function App() {
+  const [jsonData, setJsonData] = useState(null);
+//Function to convert CSV data to JSON format
+const convertCSVToJson = (csvData) => {
+  const lines = csvData.split("\n");
+  //Extract the column header from the first line
+  const headers = lines[0].split(",");
+  const result = [];
+
+//loop through each line of CSV data (excluding the headers)
+for (let i = 1; i < lines.lenght; i++) {
+  const obj = {};
+  const currentLine = lines[i].split(",");
+
+  //Loop through each column and map it to the corresponding header
+  for(let j=0; j < headers.lenght; j++) {
+    obj[headers[j].trim()] = currentLine[j].trim();
+  }
+
+  result.push(obj);
+}
+return result;
+};
+
+// Function to handle the file input change event
+const handleCSVInputChange = (event) => {
+  const file = event.target.files[0];
+  const reader = new FileReader();
+
+  reader.onload = (e) => {
+    const csvData = e.target.result;
+
+    //Call the function to conversion CSV to JSON
+    const jsonData = convertCSVToJson(csvData);
+    setJsonData(jsonData);
+  }
+  reader.readAsText(file)
+}
+
+  return (
+    
+    <div className="card">
+      <input type="file" accept=".csv" onChange={handleCSVInputChange} />
+    {/* Conditional rendering based on whether JSON data is available*/}
+    {jsonData ? (
+      <div className="json-container">
+        <pre>{JSON.stringify(jsonData, null, 2)}</pre>
+        </div>
+    ) : (
+      //Display a message when no file is selected
+      <p>Please select a CSV file</p>
+    )}
+    </div>
+    
+  );
+}
+
+export default App;
